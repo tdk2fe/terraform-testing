e8a207ed29199b42a76e2da01e58b6ef7257f029
Tim Krupinski <timothy.d.krupinski@efleets.com>
2019-10-05 22:21:35 -0500

    added egress

--- example.tf
+++ example.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "tims_sg" {
     self        = true
     protocol    = -1
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
